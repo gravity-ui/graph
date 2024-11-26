a38b363f31ee7f7a84826ce616706e9e659ec5d5
itwillwork <edgarnurullin@yandex-team.ru>
2024-11-26 11:12:25 +0300

    fix: update css after call setEntities

--- src/utils/functions/cssProp.ts
+++ src/utils/functions/cssProp.ts
@@ -1,16 +1,27 @@
 type CssVar = `--${string}`;
 
-export function setCssProp(target: HTMLElement, name: CssVar, value: string) {
+export function setCssProp(target: HTMLElement | null, name: CssVar, value: string) {
+  if (!target) {
+    return;
+  }
   target.style.setProperty(name, value);
 }
 
-export function setCssProps(target: HTMLElement, vars: { [k in CssVar]: string }) {
+export function setCssProps(target: HTMLElement | null, vars: { [k in CssVar]: string }) {
+  if (!target) {
+    return;
+  }
+
   Object.entries(vars).forEach(([name, value]) => {
     setCssProp(target, name as CssVar, value);
   });
 }
 
-export function removeCssProps(target: HTMLElement, names: CssVar[]) {
+export function removeCssProps(target: HTMLElement | null, names: CssVar[]) {
+  if (!target) {
+    return;
+  }
+
   names.forEach((name) => {
     target.style.removeProperty(name);
   });
