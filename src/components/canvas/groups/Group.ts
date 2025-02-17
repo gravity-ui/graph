4c3af85891f1599526f93037033f75f819c335f6
draedful <aschetinin@yandex-team.ru>
2025-02-17 16:51:34 +0300

    feat: Add group interaction and selection functionality

--- src/components/canvas/groups/Group.ts
+++ src/components/canvas/groups/Group.ts
@@ -1,5 +1,6 @@
 import { BlockState } from "../../../store/block/Block";
-import { getUsableRectByBlockIds } from "../../../utils/functions";
+import { getUsableRectByBlockIds, getXY } from "../../../utils/functions";
+import { EVENTS } from "../../../utils/types/events";
 import { TRect } from "../../../utils/types/shapes";
 import { GraphComponent } from "../GraphComponent";
 import { GraphLayer, TGraphLayerContext } from "../layers/graphLayer/GraphLayer";
@@ -8,13 +9,74 @@ export type TGroupProps = {
   id: string;
 };
 
-export class Group extends GraphComponent<TGroupProps, { rect: TRect }, TGraphLayerContext> {
+export class Group extends GraphComponent<TGroupProps, { rect: TRect; selected: boolean }, TGraphLayerContext> {
   public declare context: TGraphLayerContext;
   protected blocks: BlockState[] = [];
+  protected lastDragEvent?: MouseEvent;
+  protected startDragCoords: number[] = [];
+
+  public cursor = "pointer";
 
   constructor(props: TGroupProps, parent: GraphLayer) {
     super(props, parent);
     this.subscribeToBlocks();
+
+    this.addEventListener(EVENTS.DRAG_START, this);
+    this.addEventListener(EVENTS.DRAG_UPDATE, this);
+    this.addEventListener(EVENTS.DRAG_END, this);
+    this.addEventListener("click", this);
+  }
+
+  public handleEvent(event: CustomEvent) {
+    switch (event.type) {
+      case EVENTS.DRAG_START: {
+        this.onDragStart(event.detail.sourceEvent);
+        break;
+      }
+      case EVENTS.DRAG_UPDATE: {
+        this.onDragUpdate(event.detail.sourceEvent);
+        break;
+      }
+      case EVENTS.DRAG_END: {
+        this.onDragEnd();
+        break;
+      }
+      case "click": {
+        this.setState({ selected: !this.state.selected });
+        break;
+      }
+    }
+  }
+
+  protected onDragStart(event: MouseEvent) {
+    this.lastDragEvent = event;
+    const xy = getXY(this.context.canvas, event);
+    this.startDragCoords = this.context.camera.applyToPoint(xy[0], xy[1]);
+  }
+
+  protected onDragUpdate(event: MouseEvent) {
+    if (!this.startDragCoords.length) return;
+
+    this.lastDragEvent = event;
+    const [canvasX, canvasY] = getXY(this.context.canvas, event);
+    const [cameraSpaceX, cameraSpaceY] = this.context.camera.applyToPoint(canvasX, canvasY);
+
+    const diffX = (this.startDragCoords[0] - cameraSpaceX) | 0;
+    const diffY = (this.startDragCoords[1] - cameraSpaceY) | 0;
+
+    // Обновляем позиции всех блоков в группе
+    this.blocks.forEach((block) => {
+      const nextX = block.x - diffX;
+      const nextY = block.y - diffY;
+      block.updateXY(nextX, nextY);
+    });
+
+    this.startDragCoords = [cameraSpaceX, cameraSpaceY];
+  }
+
+  protected onDragEnd() {
+    this.lastDragEvent = undefined;
+    this.startDragCoords = [];
   }
 
   protected subscribeToBlocks() {
@@ -33,7 +95,7 @@ export class Group extends GraphComponent<TGroupProps, { rect: TRect }, TGraphLa
           height: rect.height + padding * 2,
         };
 
-        this.setState({ rect: newRect });
+        this.setState({ rect: newRect, selected: this.state?.selected || false });
         this.updateHitBox(newRect);
       }
       this.performRender();
@@ -51,9 +113,9 @@ export class Group extends GraphComponent<TGroupProps, { rect: TRect }, TGraphLa
     const rect = this.state.rect;
 
     // Настраиваем стиль для группы
-    ctx.strokeStyle = "rgba(100, 100, 100, 0.3)";
-    ctx.fillStyle = "rgba(100, 100, 100, 0.1)";
-    ctx.lineWidth = 2;
+    ctx.strokeStyle = this.state.selected ? "rgba(100, 100, 100, 0.6)" : "rgba(100, 100, 100, 0.3)";
+    ctx.fillStyle = this.state.selected ? "rgba(100, 100, 100, 0.2)" : "rgba(100, 100, 100, 0.1)";
+    ctx.lineWidth = this.state.selected ? 3 : 2;
 
     // Рисуем прямоугольник группы
     ctx.beginPath();
@@ -62,7 +124,7 @@ export class Group extends GraphComponent<TGroupProps, { rect: TRect }, TGraphLa
     ctx.stroke();
 
     // Рисуем название группы
-    ctx.fillStyle = "rgba(100, 100, 100, 0.7)";
+    ctx.fillStyle = this.state.selected ? "rgba(100, 100, 100, 0.9)" : "rgba(100, 100, 100, 0.7)";
     ctx.font = "14px Arial";
     ctx.textAlign = "left";
     ctx.fillText(`Group: ${this.props.id}`, rect.x + 10, rect.y + 20);
