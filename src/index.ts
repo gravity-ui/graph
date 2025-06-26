2e6084a55c5a5efd0b3cce3978ee08d79598a7d7
Maksim Efremov <ma-efremoff@yandex-team.ru>
2025-06-26 17:10:22 +0300

    chore: add export of Component

--- src/index.ts
+++ src/index.ts
@@ -24,3 +24,4 @@ export * from "./components/canvas/groups";
 
 export * from "./components/canvas/layers/newBlockLayer/NewBlockLayer";
 export * from "./components/canvas/layers/connectionLayer/ConnectionLayer";
+export * from "./lib/Component";
