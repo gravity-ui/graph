1c40366d68c30e42ed500ca525b2515d03211165
draedful <aschetinin@yandex-team.ru>
2025-08-16 23:04:42 +0300

    ...

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,5 @@
 import type { Preview } from "@storybook/react-webpack5";
 import React, { StrictMode } from "react";
-import { ThemeProvider } from "@gravity-ui/uikit";
 
 import './styles/global.css';
 
@@ -8,9 +7,7 @@ const preview: Preview = {
     decorators: [
       (Story) => (
         <StrictMode>
-          <ThemeProvider>
-            <Story />
-          </ThemeProvider>
+          <Story />
         </StrictMode>
       ),
     ],
