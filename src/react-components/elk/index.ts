0c1439320703c3f60a067e0fa163405af0032f37
simbiozizv <simbiozizv@yandex-team.ru>
2026-02-19 12:03:14 +0300

    chore(Connection): new multipont connection type

--- src/react-components/elk/index.ts
+++ src/react-components/elk/index.ts
@@ -1,2 +1,7 @@
 export { MultipointConnection } from "./components/MultipointConnection";
+export {
+  BezierMultipointConnection,
+  generateBezierParams,
+  bezierCurveLine,
+} from "./components/BezierMultipointConnection";
 export { useElk } from "./hooks/useElk";
