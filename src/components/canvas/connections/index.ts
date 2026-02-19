2a4176796699cfdfe64fc58b25a6e4178ded1891
simbiozizv <simbioziz@gmail.com>
2026-02-19 15:44:00 +0300

    chore(Connection): move multipoint connections

--- src/components/canvas/connections/index.ts
+++ src/components/canvas/connections/index.ts
@@ -1,4 +1,6 @@
 export * from "./BaseConnection";
 export * from "./BlockConnection";
+export * from "./MultipointConnection";
+export * from "./BezierMultipointConnection";
 export * from "./Arrow";
 export * from "./BatchPath2D";
--- src/react-components/elk/types/index.ts
+++ src/react-components/elk/types/index.ts
@@ -1,18 +1,5 @@
-import { TConnection, TConnectionId } from "../../../store/connection/ConnectionState";
+import { TConnectionId, TMultipointConnection } from "../../../store/connection/ConnectionState";
 import { TPoint } from "../../../utils/types/shapes";
-
-export type TLabel = {
-  height?: number;
-  width?: number;
-  x?: number;
-  y?: number;
-  text?: string;
-};
-
-export type TMultipointConnection = TConnection & {
-  points?: TPoint[];
-  labels?: TLabel[];
-};
 
 export type ConverterResult = {
   edges: Record<TConnectionId, Pick<TMultipointConnection, "points" | "labels">>;
